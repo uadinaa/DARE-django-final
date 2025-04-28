1317dcdf1f7e98633d44433974306249880e77b1
Amina Amanzholova <eomino1607@gmail.com>
2025-04-29 00:10:06 +0500

    cors changing and new register form in frontend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
