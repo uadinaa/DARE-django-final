017f525456313d2028f37ba61f25b9b5b69772d5
Dyletan <eldar1016@gmail.com>
2025-05-12 12:41:56 +0500

    redeploy backend

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -11,6 +11,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     )  # Добавлено email
     role_display = serializers.CharField(source="get_role_display", read_only=True)
     avatar_url = serializers.SerializerMethodField()
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField() 
 
     class Meta:
         model = Profile
@@ -23,14 +25,18 @@ class ProfileSerializer(serializers.ModelSerializer):
             'is_blocked',
             'can_monetize_posts',
             'level_score',
-            'levels_last_calculated_at'
+            'levels_last_calculated_at',
+            'followers_count',
+            'following_count'
         ]
         read_only_fields = [
             'id', 'username',
             'role', 'email',
             'role_display',
             'is_blocked',
-            'avatar_url'
+            'avatar_url',
+            'followers_count',
+            'following_count'
             ]
         # Сделаем 'avatar' write_only, если отдаем только 'avatar_url'
         extra_kwargs = {"avatar": {"write_only": True}}
@@ -43,6 +49,16 @@ class ProfileSerializer(serializers.ModelSerializer):
             except ValueError:
                 return obj.avatar.url
         return "https://fitness-platform-media.s3.eu-north-1.amazonaws.com/media/default/default-avatar-icon.jpg"
+    
+    def get_followers_count(self, obj):
+        if hasattr(obj.user, 'followers'):
+            return obj.user.followers.count()
+        return 0
+
+    def get_following_count(self, obj):
+        if hasattr(obj.user, 'following'):
+            return obj.user.following.count()
+        return 0
 
 
 # UserSerializer использует ProfileSerializer выше
